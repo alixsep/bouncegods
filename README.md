10d6052551a028122db0c010e5a2e2a163632cf9
alixsep <alixsep@outlook.com>
2021-08-21 19:51:53 +0430

    Initial Release

---
+++ README.md
@@ -0,0 +1,1 @@
+The Website for Bouncegods label is under construction.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      The Website for Bouncegods label is under construction.
+    </div>
+  );
+}
+
+export default App;
